d6e6e411deb56ea2d6df722bfa5b1970e33a7030
GM Bellal <gm.bellalhossain@gmail.com>
2017-08-08 17:44:21 +0600

    Some Issues Fixed too.

---
+++ application/views/users/users_form.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+    <head>
+        <title>harviacode.com - codeigniter crud generator</title>
+        <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>"/>
+        <style>
+            body{
+                padding: 15px;
+            }
+        </style>
+    </head>
+    <body>
+        <h2 style="margin-top:0px">Users <?php echo $button ?></h2>
+        <form action="<?php echo $action; ?>" method="post">
+	    <div class="form-group">
+            <label for="varchar">Username <?php echo form_error('username') ?></label>
+            <input type="text" class="form-control" name="username" id="username" placeholder="Username" value="<?php echo $username; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Password <?php echo form_error('password') ?></label>
+            <input type="text" class="form-control" name="password" id="password" placeholder="Password" value="<?php echo $password; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="int">Age <?php echo form_error('age') ?></label>
+            <input type="text" class="form-control" name="age" id="age" placeholder="Age" value="<?php echo $age; ?>" />
+        </div>
+	    <input type="hidden" name="id" value="<?php echo $id; ?>" /> 
+	    <button type="submit" class="btn btn-primary"><?php echo $button ?></button> 
+	    <a href="<?php echo site_url('users') ?>" class="btn btn-default">Cancel</a>
+	</form>
+    </body>
+</html>
